2fa1a2237fa84fa3916c3fcc3119fec2089f9907
Joseff708 <brasaurez@uniboyaca.edu.co>
2025-05-28 20:22:05 -0500

    no me acuerdo

--- login_microservices/routes/api.php
+++ login_microservices/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HistoriaUsuarioController;
+
+Route::apiResource('historias', HistoriaUsuarioController::class);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
